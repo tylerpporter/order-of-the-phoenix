f36cbfdd4389ae2580992a240d99c15957f0d3ef
Tyler Porter <tylerpporter@gmail.com>
2020-05-19 10:43:20 -0600

    Refactor tests

--- spec/features/user_can_find_members_of_oop_spec.rb
+++ spec/features/user_can_find_members_of_oop_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 describe "As a user when I visit '/' and select Gryffindor and click Search For Members" do
-  scenario "I should be on page '/search'" do
+  before :each do
     visit '/'
     click_on "Search For Members"
+  end
+  scenario "I should be on page '/search'" do
 
     expect(current_path).to eq('/search')
   end
   scenario 'I should see total number of OOTP members that belong to Gryffindor' do
-    visit '/'
-    click_on "Search For Members"
 
     expect(page).to have_content("Total Members: 21")
     expect(page).to have_css('.member', count: 21)
   end
   scenario 'I should see attributes of each member' do
-    visit '/'
-    click_on "Search For Members"
 
     within first('.member') do
       expect(page).to have_content("Sirius Black")
