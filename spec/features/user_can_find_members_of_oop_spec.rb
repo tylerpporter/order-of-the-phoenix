325f0c7bf7ad048d2d339c108a9c2cdc69424b9d
Tyler Porter <tylerpporter@gmail.com>
2020-05-19 10:32:32 -0600

    Finish writing tests

--- spec/features/user_can_find_members_of_oop_spec.rb
+++ spec/features/user_can_find_members_of_oop_spec.rb
@@ -1,14 +1,35 @@
 require 'rails_helper'
 
-describe 'As a user when I visit / and select Gryffindor and click Search For Members' do
-  scenario 'I should be on page /search' do
+describe "As a user when I visit '/' and select Gryffindor and click Search For Members" do
+  scenario "I should be on page '/search'" do
     visit '/'
     click_on "Search For Members"
 
     expect(current_path).to eq('/search')
   end
-  xscenario 'I should see total number of OOTP members that belong to Gryffindor' do
+  scenario 'I should see total number of OOTP members that belong to Gryffindor' do
+    visit '/'
+    click_on "Search For Members"
+
+    expect(page).to have_content("Total Members: 21")
+    expect(page).to have_css('.member', count: 21)
   end
-  xscenario 'I should see attributes of each member' do
+  scenario 'I should see attributes of each member' do
+    visit '/'
+    click_on "Search For Members"
+
+    within first('.member') do
+      expect(page).to have_content("Sirius Black")
+      expect(page).to have_content("House: Gryffindor")
+      expect(page).to_not have_content("Role: ")
+      expect(page).to_not have_content("Patronus: ")
+    end
+
+    within all('.member').last do
+      expect(page).to have_content("Ronald Weasley")
+      expect(page).to have_content("House: Gryffindor")
+      expect(page).to have_content("Role: student")
+      expect(page).to have_content("Patronus: jack russell terrier")
+    end
   end
 end
