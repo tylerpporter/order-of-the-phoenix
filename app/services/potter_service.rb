3b2a5c0bc292ea552209bc5c93a83e3497bbcec7
Tyler Porter <tylerpporter@gmail.com>
2020-05-19 10:34:36 -0600

    Create potter service to connect to api

---
+++ app/services/potter_service.rb
@@ -0,0 +1,12 @@
+class PotterService
+  class << self
+    def conn(house)
+      resp = Faraday.get('https://www.potterapi.com/v1/characters') do |req|
+        req.params[:key] = ENV["POTTER_KEY"]
+        req.params[:house] = house
+        req.params[:orderOfThePhoenix] = true
+      end
+      JSON.parse(resp.body, symbolize_names: true)
+    end
+  end
+end
